e977db9191a724def5aaafba85c70f032db039e2
Haancock <Haancoock123@gmail.com>
2020-08-16 18:21:47 +0300

    finished profile page

---
+++ pages/profile.vue
@@ -0,0 +1,39 @@
+<template>
+    <v-layout
+    column
+    justify-center
+    align-center
+    >
+        <v-card
+            class="mx-auto"
+            max-width="544"
+        >
+            <v-img
+            :src="data.image"
+            height="500px"
+            ></v-img>
+    
+            <v-card-title>
+                <div class="name">{{data.name}}</div>
+            </v-card-title>
+
+            <v-card-subtitle>
+                <div class="email">{{data.email}}</div>
+            </v-card-subtitle>
+
+        </v-card>
+        <nuxt-link to="/">Выйти</nuxt-link>
+    </v-layout>
+
+</template>
+
+<script>
+  export default {
+    data: () => ({
+        data: {}
+    }),
+    async mounted() {
+        this.data = await this.$axios.$get('data.json')
+    }
+  }
+</script>
